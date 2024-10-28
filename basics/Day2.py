4f412af098030c71c415407201fb3806bd2efa3b
Shirajhussain <shirajhussain2005@gmail.com>
2024-10-28 10:09:33 +0530

    Day2

---
+++ basics/Day2.py
@@ -0,0 +1,24 @@
+A = float(input("Enter the value of A :"))
+B = float(input("Enter the value of B :"))
+
+sum = A + B
+print("Sum is :",sum)
+
+A = float(input("Enter the value of A :"))
+B = float(input("Enter the value of B :"))
+
+sub = A - B
+print("Subtraction is :",sub)
+
+A = float(input("Enter the value of A :"))
+B = float(input("Enter the value of B :"))
+
+Mul = A * B
+print("Multiplication is :",Mul)
+
+
+A = float(input("Enter the value of A :"))
+B = float(input("Enter the value of B :"))
+
+Div = A / B
+print("Division is :",Div)
