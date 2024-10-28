efb405f1e6704de10c06f372dda0df964b0c7c4d
Shirajhussain <shirajhussain2005@gmail.com>
2024-10-28 09:51:39 +0530

    slic.py

---
+++ basics/slic.py
@@ -0,0 +1,25 @@
+str = "Hello, World!"
+sub_str = str[7:12]
+print(" substring:", sub_str)
+
+
+str = "Python"
+reversed_str = str[::-1]
+print("Reversed string:", reversed_str)
+
+
+str = "abcdefgh"
+skipped_str = str[::2]
+print("String with every second character:", skipped_str)
+
+str = "OpenAI"
+modified_str = str[2:]
+print("String after removing first 2 characters:", modified_str)
+
+
+
+str = "SDET"
+first_char = str[0]
+last_char = str[-1]
+print("First character:", first_char)
+print("Last character:", last_char)
--- basics/test.py
+++ basics/test.py
@@ -1,1 +1,3 @@
-print("test")
+str = "Hello, World!"
+sub_str = str[7:12]  # Starting at index 7 up to, but not including, index 12
+print("Extracted substring:", sub_str
